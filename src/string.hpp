dc717f6e40303298369c151ad21415e34796698b
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-04-25 21:40:03 -0700

    Update string.hpp

--- src/string.hpp
+++ src/string.hpp
@@ -1,66 +1,61 @@
 #ifndef STRING_HPP
 #define STRING_HPP
 
-#include <iostream>
-#include <cstring>
+#include <iosfwd>
 
 constexpr int MAXLEN = 1024;
 
 class String {
+public:
+    explicit String(const char* s = "");
 
-public: 
-	explicit String(const char* s = "");
-	String(const String& s);
-	String& operator =(const String& s);
-	char& operator [] (int index);
-	int size () const; //change 1
-	String reverse ()const; //change 2
-	int indexOf(char c)const;
-	int indexOf(const String& s)const;
+    String(const String& s);
 
-	bool operator==(const String& s) const;
-	bool operator!=(const String& s) const;
-	bool operator>(const String& s) const;
-	bool operator<(const String& s) const;
-	bool operator<=(const String& s) const;
-	bool operator>=(const String& s) const;
+    String& operator=(const String& s);
 
-	String operator+(const String& s)const;
-	String& operator+=(const String& s);
+    char& operator[](int index);
 
-	void print(std::ostream& out) const;
+    int size();
 
-	void read(std::istream& in);
+    String reverse();
 
-	~String();
+    int indexOf(char c);
 
-	bool in_bounds(int i) const
-	{
-		return i >= 0 && i < strlen(buf);
-	}
+    int indexOf(const String& s);
 
-	static int strlen(const char* s);
-	static char *strcpy(char* dest, const char* src);
-	static char *strncpy(char* dest, const char* src, int n);
-	static char* strncat(char* dest, const char* src, int n);
-	static int strcmp(const char* left, const char* right);
-	static int strncmp(const char* left, const char* right, int n);
-	static void reverse_cpy(char* dest, const char* src);
-	static const char *strchr(const char* str, char c);
-	static const char *strstr(const char* haystack, const char* needle);
-	static char* strcat(char* dest, const char* src);
+    bool operator==(const String& s) const;
+    bool operator!=(const String& s) const;
+    bool operator<(const String& s) const;
+    bool operator<=(const String& s) const;
+    bool operator>(const String& s) const;
+    bool operator>=(const String& s) const;
 
+    String operator+(const String& s) const;
+
+    String& operator+=(const String& s);
+
+    void print(std::ostream& out) const;
+
+    void read(std::istream& in);
+
+    ~String();
+
+    bool in_bounds(int i);
+    static int strlen(const char* s);
+    static char* strcpy(char* dest, const char* src);
+    static char* strncpy(char* dest, const char* src, int n);
+    static char* strcat(char* dest, const char* src);
+    static char* strncat(char* dest, const char* src, int n);
+    static int strcmp(const char* left, const char* right);
+    static int strncmp(const char* left, const char* right, int n);
+    static void reverse_copy(char* dest, const char* src);
+    static const char* strchr(const char* str, char c);
+    static const char* strstr(const char* haystack, const char* needle);
 
 private:
-	char buf[MAXLEN];
-
+    char buf[MAXLEN];
 };
 
 std::ostream& operator<<(std::ostream& out, const String& s);
 std::istream& operator>>(std::istream& in, String& s);
-
-
-
 #endif
-
-
